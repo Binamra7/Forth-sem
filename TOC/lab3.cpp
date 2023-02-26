d3f2e806b7c1dc1ee7dcd5a4de7549b8ec02eb6c
Binamra7 <binamra7khadka6@gmail.com>
2023-02-26 15:52:01 +0545

    Add(TOC): lab 3
    NFA c++ code

--- TOC/lab3.cpp
+++ TOC/lab3.cpp
@@ -1,9 +1,57 @@
-#include<iostream>
+#include <iostream>
+#include <bits/stdc++.h>
 
 using namespace std;
 
+int current = 0;
+
+void q0(char c)
+{
+    if (c == 'a')
+        current = 1;
+    else
+    	current = -1;
+}
+
+void q1(char c)
+{
+    if (c == 'a')
+        current = 1;
+    else if (c == 'b')
+    	current = 2;
+    else current = -1;
+}
+
 int main()
 {
-  
-  return 0;
+    string str;
+    char c;
+    int i = 0;
+    cout << "Enter the string: " << endl;
+    cin >> str;
+    c = str[i];
+
+    while (c != '\0')
+    {
+        switch (current)
+        {
+        	case 0:
+            	q0(c);
+            	break;
+        	case 1:
+            	q1(c);
+            	break;
+            default:
+            	break;
+        }
+        c = str[++i];
+    }
+
+    if (current == 2)
+        printf("accepted");
+    else
+        printf("rejected");
+
+    return 0;
 }
+
